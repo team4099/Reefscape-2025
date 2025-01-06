977fab70c87ceae706c35e269369b467a1bc8e2c
yamamara <yamamrtyu@gmail.com>
2025-01-06 14:42:58 -0500

    Update .gitignore + build constants

--- src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
+++ src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
@@ -6,10 +6,10 @@ package com.team4099.robot2025
 const val MAVEN_GROUP = ""
 const val MAVEN_NAME = "Reefscape-2025"
 const val VERSION = "unspecified"
-const val GIT_REVISION = 11
-const val GIT_SHA = "b65ebe2ca4fc2092af1022dcb656e5bac35dbab5"
-const val GIT_DATE = "2025-01-06T11:52:12Z"
+const val GIT_REVISION = 12
+const val GIT_SHA = "3f3a2fafccf7ae09a8c0294a47c8b8723e8ace7c"
+const val GIT_DATE = "2025-01-06T13:34:40Z"
 const val GIT_BRANCH = "main"
-const val BUILD_DATE = "2025-01-06T13:27:24Z"
-const val BUILD_UNIX_TIME = 1736188044827L
+const val BUILD_DATE = "2025-01-06T14:27:41Z"
+const val BUILD_UNIX_TIME = 1736191661903L
 const val DIRTY = 1
