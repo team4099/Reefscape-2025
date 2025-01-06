3f3a2fafccf7ae09a8c0294a47c8b8723e8ace7c
yamamara <yamamrtyu@gmail.com>
2025-01-06 13:34:40 -0500

    Update PathPlanner + fix version error

--- src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
+++ src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
@@ -6,10 +6,10 @@ package com.team4099.robot2025
 const val MAVEN_GROUP = ""
 const val MAVEN_NAME = "Reefscape-2025"
 const val VERSION = "unspecified"
-const val GIT_REVISION = 10
-const val GIT_SHA = "51b6884ddbc0e6c036fd5af76e119c8b85e9cd92"
-const val GIT_DATE = "2025-01-06T10:31:31Z"
+const val GIT_REVISION = 11
+const val GIT_SHA = "b65ebe2ca4fc2092af1022dcb656e5bac35dbab5"
+const val GIT_DATE = "2025-01-06T11:52:12Z"
 const val GIT_BRANCH = "main"
-const val BUILD_DATE = "2025-01-06T11:51:53Z"
-const val BUILD_UNIX_TIME = 1736182313631L
+const val BUILD_DATE = "2025-01-06T13:27:24Z"
+const val BUILD_UNIX_TIME = 1736188044827L
 const val DIRTY = 1
