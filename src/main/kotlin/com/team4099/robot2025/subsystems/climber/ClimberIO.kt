7cf4521405fbab87d174b0417ab3618bdbcf2d7c
yamamara <yamamrtyu@gmail.com>
2025-01-12 00:07:14 -0500

    Add logged variables to ClimberIO

--- src/main/kotlin/com/team4099/robot2025/subsystems/climber/ClimberIO.kt
+++ src/main/kotlin/com/team4099/robot2025/subsystems/climber/ClimberIO.kt
@@ -8,15 +8,19 @@ import org.team4099.lib.units.base.inAmperes
 import org.team4099.lib.units.base.inCelsius
 import org.team4099.lib.units.derived.*
 import org.team4099.lib.units.inRadiansPerSecond
+import org.team4099.lib.units.inRadiansPerSecondPerSecond
 import org.team4099.lib.units.perSecond
 
 interface ClimberIO {
     class ClimberInputs: LoggableInputs {
         var climberPosition = 0.0.radians
         var climberVelocity = 0.0.radians.perSecond
+        var climberAcceleration = 0.0.radians.perSecond.perSecond
+        var climberTorque = 0.0
         var climberAppliedVoltage = 0.0.volts
-        var climberSupplyCurrent = 0.0.amps
+        var climberDutyCycle = 0.0.volts
         var climberStatorCurrent = 0.0.amps
+        var climberSupplyCurrent = 0.0.amps
         var climberTemperature = 0.0.celsius
 
         var isSimulated = false
@@ -24,18 +28,34 @@ interface ClimberIO {
         override fun toLog(table: LogTable) {
             table.put("climberPosition", climberPosition.inRadians)
             table.put("climberVelocity", climberVelocity.inRadiansPerSecond)
+            table.put("climberAcceleration", climberAcceleration.inRadiansPerSecondPerSecond)
+            table.put("climberTorque", climberTorque)
             table.put("climberAppliedVoltage", climberAppliedVoltage.inVolts)
-            table.put("climberSupplyCurrent", climberSupplyCurrent.inAmperes)
+            table.put("climberDutyCycle", climberDutyCycle.inVolts)
             table.put("climberStatorCurrent", climberStatorCurrent.inAmperes)
+            table.put("climberSupplyCurrent", climberSupplyCurrent.inAmperes)
             table.put("climberTemperature", climberTemperature.inCelsius)
         }
 
         override fun fromLog(table: LogTable?) {
             table?.get("climberPositionRadians", climberPosition.inRadians)?.let { climberPosition = it.radians }
-            table?.get("climberVelocityRadiansPerSecond", climberVelocity.inRadiansPerSecond)?.let { climberVelocity = it.radians.perSecond }
+
+            table?.get("climberVelocityRadiansPerSecond", climberVelocity.inRadiansPerSecond)?.let {
+                climberVelocity = it.radians.perSecond
+            }
+
+            table?.get(
+                "climberAccelerationRadiansPerSecondPerSecond",
+                climberAcceleration.inRadiansPerSecondPerSecond
+            )?.let {
+                climberAcceleration = it.radians.perSecond.perSecond
+            }
+
+            table?.get("climberTorque", climberTorque)?.let { climberTorque = it }
             table?.get("climberAppliedVolts", climberAppliedVoltage.inVolts)?.let { climberAppliedVoltage = it.volts }
-            table?.get("climberSupplyCurrentAmps", climberSupplyCurrent.inAmperes)?.let { climberSupplyCurrent = it.amps }
+            table?.get("climberDutyCycleVolts", climberDutyCycle.inVolts)?.let { climberDutyCycle = it.volts }
             table?.get("climberStatorCurrentAmps", climberStatorCurrent.inAmperes)?.let { climberStatorCurrent = it.amps }
+            table?.get("climberSupplyCurrentAmps", climberSupplyCurrent.inAmperes)?.let { climberSupplyCurrent = it.amps }
             table?.get("climberTemperatureCelsius", climberTemperature.inCelsius)?.let { climberTemperature = it.celsius }
         }
     }
