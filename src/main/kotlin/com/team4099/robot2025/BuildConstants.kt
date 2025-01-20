a133d7de76526afe7f4363028323c6aa4ab5d3bf
Matthew Choulas <79815700+MatthewChoulas@users.noreply.github.com>
2025-01-20 03:18:14 -0500

    Revert "Arm code done I think (#2)"
    This reverts commit 8a961c96dbef4f09a891d513672d2b5f3d1a0125.

--- src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
+++ src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
@@ -6,10 +6,10 @@ package com.team4099.robot2025
 const val MAVEN_GROUP = ""
 const val MAVEN_NAME = "Reefscape-2025"
 const val VERSION = "unspecified"
-const val GIT_REVISION = 26
-const val GIT_SHA = "9dcfdd378a5a7e1769e659f39458b9f380d2408b"
-const val GIT_DATE = "2025-01-15T15:37:57Z"
-const val GIT_BRANCH = "manipulator"
-const val BUILD_DATE = "2025-01-17T14:56:51Z"
-const val BUILD_UNIX_TIME = 1737143811712L
+const val GIT_REVISION = 22
+const val GIT_SHA = "6c34e77cd52c4e02e1307662835c5ab7c6ddcc13"
+const val GIT_DATE = "2025-01-10T21:45:15Z"
+const val GIT_BRANCH = "elevator"
+const val BUILD_DATE = "2025-01-11T21:49:15Z"
+const val BUILD_UNIX_TIME = 1736650155288L
 const val DIRTY = 1
--- src/main/kotlin/com/team4099/robot2025/subsystems/superstructure/Request.kt
+++ src/main/kotlin/com/team4099/robot2025/subsystems/superstructure/Request.kt
@@ -1,13 +1,10 @@
 package com.team4099.robot2025.subsystems.superstructure
 
-import com.team4099.robot2025.config.constants.ArmConstants
 import edu.wpi.first.math.kinematics.ChassisSpeeds
 import org.team4099.lib.units.AngularVelocity
 import org.team4099.lib.units.LinearVelocity
-import org.team4099.lib.units.derived.Angle
 import org.team4099.lib.units.base.Length
 import org.team4099.lib.units.derived.ElectricalPotential
-import org.team4099.lib.units.derived.degrees
 
 sealed interface Request {
 
@@ -33,23 +30,9 @@ sealed interface Request {
     class Characterize(val voltage: ElectricalPotential) : DrivetrainRequest
   }
 
-  sealed interface ArmRequest : Request {
-    class OpenLoop(val armVoltage: ElectricalPotential) : ArmRequest
-    class CloseLoop(
-      val armPosition: Angle
-    ) : ArmRequest
-    class Zero() : ArmRequest
-  }
-
-  sealed interface RollersRequest : Request {
-    class OpenLoop(val RollersVoltage: ElectricalPotential) : RollersRequest
-    class Zero(): RollersRequest
-
   sealed interface ElevatorRequest : Request {
     class ClosedLoop(val position: Length) : ElevatorRequest
     class OpenLoop(val voltage: ElectricalPotential) : ElevatorRequest
     class Home() : ElevatorRequest
   }
 }
-
-
