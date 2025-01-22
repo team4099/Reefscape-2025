2ec6f255b7ec6f41f50e8007544fd5b0ba1a3663
Shom770 <shayaanwadkar@gmail.com>
2025-01-22 16:24:57 -0500

    example path auto

--- src/main/kotlin/com/team4099/robot2025/auto/mode/ExamplePathAuto.kt
+++ src/main/kotlin/com/team4099/robot2025/auto/mode/ExamplePathAuto.kt
@@ -2,7 +2,6 @@ package com.team4099.robot2025.auto.mode
 
 import choreo.Choreo
 import choreo.trajectory.SwerveSample
-import com.team4099.lib.trajectory.FieldWaypoint
 import com.team4099.robot2025.commands.drivetrain.DrivePathCommand
 import com.team4099.robot2025.subsystems.drivetrain.drive.Drivetrain
 import com.team4099.robot2025.util.TrajectoryTypes
@@ -11,7 +10,6 @@ import org.team4099.lib.geometry.Pose2d
 import org.team4099.lib.geometry.Translation2d
 import org.team4099.lib.units.base.meters
 import org.team4099.lib.units.derived.degrees
-import org.team4099.lib.units.derived.inRotation2ds
 
 class ExamplePathAuto(val drivetrain: Drivetrain) : SequentialCommandGroup() {
   init {
