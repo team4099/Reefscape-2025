807dec980954b4eead59ba9d7f724afc39166e13
Shom770 <shayaanwadkar@gmail.com>
2025-01-27 16:48:41 -0500

    6 coral?

--- src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
+++ src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
@@ -6,10 +6,10 @@ package com.team4099.robot2025
 const val MAVEN_GROUP = ""
 const val MAVEN_NAME = "Reefscape-2025"
 const val VERSION = "unspecified"
-const val GIT_REVISION = -1
-const val GIT_SHA = "UNKNOWN"
-const val GIT_DATE = "UNKNOWN"
-const val GIT_BRANCH = "UNKNOWN"
-const val BUILD_DATE = "2025-01-24T13:06:33Z"
-const val BUILD_UNIX_TIME = 1737741993843L
-const val DIRTY = 69
+const val GIT_REVISION = 39
+const val GIT_SHA = "41c532275e84407672a27bc1e0d7460fe8844570"
+const val GIT_DATE = "2025-01-25T10:57:57Z"
+const val GIT_BRANCH = "choreo-paths"
+const val BUILD_DATE = "2025-01-26T16:49:08Z"
+const val BUILD_UNIX_TIME = 1737928148970L
+const val DIRTY = 1
