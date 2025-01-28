a648833d3b3cc48d82578244f3ec89d412c4f4a8
Shom770 <shayaanwadkar@gmail.com>
2025-01-28 18:13:07 -0500

    fix rebase

--- src/main/kotlin/com/team4099/robot2025/subsystems/superstructure/Request.kt
+++ src/main/kotlin/com/team4099/robot2025/subsystems/superstructure/Request.kt
@@ -81,4 +81,11 @@ sealed interface Request {
     class OpenLoop(val voltage: ElectricalPotential) : ElevatorRequest
     class Home() : ElevatorRequest
   }
+
+  sealed interface ElevatorRequest : Request {
+    class ClosedLoop(val position: Length) : ElevatorRequest
+    class OpenLoop(val voltage: ElectricalPotential) : ElevatorRequest
+    class Home() : ElevatorRequest
+  }
+
 }
