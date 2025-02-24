51062aab539f7378c3649e70a6f6af1292e63aa9
Shom770 <shayaanwadkar@gmail.com>
2025-02-24 08:48:08 -0500

    keep reabse

--- src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
+++ src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
@@ -6,7 +6,6 @@ package com.team4099.robot2025
 const val MAVEN_GROUP = ""
 const val MAVEN_NAME = "Reefscape-2025"
 const val VERSION = "unspecified"
-<<<<<<< HEAD
 const val GIT_REVISION = 68
 const val GIT_SHA = "1fa9d690d5c376097849c0904cff584af6d30ecd"
 const val GIT_DATE = "2025-02-22T10:17:09Z"
@@ -14,23 +13,3 @@ const val GIT_BRANCH = "superstructure"
 const val BUILD_DATE = "2025-02-22T18:37:30Z"
 const val BUILD_UNIX_TIME = 1740267450857L
 const val DIRTY = 1
-=======
-<<<<<<< HEAD
-
-const val GIT_REVISION = 40
-const val GIT_SHA = "807dec980954b4eead59ba9d7f724afc39166e13"
-const val GIT_DATE = "2025-01-27T16:48:41Z"
-const val GIT_BRANCH = "choreo-paths"
-const val BUILD_DATE = "2025-01-28T10:13:34Z"
-const val BUILD_UNIX_TIME = 1738077214904L
-
-=======
-const val GIT_REVISION = 34
-const val GIT_SHA = "931c08d700e2a6ba68bb7a883ada7e5a94c37cd9"
-const val GIT_DATE = "2025-01-26T20:18:39Z"
-const val GIT_BRANCH = "main"
-const val BUILD_DATE = "2025-01-27T19:03:59Z"
-const val BUILD_UNIX_TIME = 1738022639805L
->>>>>>> 518d932 (get dt running)
-const val DIRTY = 1
->>>>>>> f1923c0 (get dt running)
