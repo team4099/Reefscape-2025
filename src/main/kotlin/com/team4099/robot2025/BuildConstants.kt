4fdac9ee9b1e50b7218b9c8f93acde3e540dfcaf
Shom770 <shayaanwadkar@gmail.com>
2025-03-06 17:09:59 -0500

    new code

--- src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
+++ src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
@@ -6,10 +6,10 @@ package com.team4099.robot2025
 const val MAVEN_GROUP = ""
 const val MAVEN_NAME = "Reefscape-2025"
 const val VERSION = "unspecified"
-const val GIT_REVISION = 80
-const val GIT_SHA = "2d59ca5b911308cd9cc19190cb0e6cf2c2ce8ca0"
-const val GIT_DATE = "2025-03-06T16:38:32Z"
-const val GIT_BRANCH = "HEAD"
-const val BUILD_DATE = "2025-03-06T16:54:32Z"
-const val BUILD_UNIX_TIME = 1741298072424L
+const val GIT_REVISION = 76
+const val GIT_SHA = "b28f72459b103c12f88f75f36bdf369e7e55dba2"
+const val GIT_DATE = "2025-03-06T17:04:45Z"
+const val GIT_BRANCH = "superstructure"
+const val BUILD_DATE = "2025-03-06T17:07:37Z"
+const val BUILD_UNIX_TIME = 1741298857777L
 const val DIRTY = 1
---
+++ src/main/kotlin/com/team4099/robot2025/subsystems/rollers/Rollers.kt
@@ -0,0 +1,104 @@
+package com.team4099.robot2025.subsystems.rollers
+
+import com.team4099.lib.hal.Clock
+import com.team4099.robot2025.config.constants.RollersConstants
+import com.team4099.robot2025.subsystems.superstructure.Request
+import com.team4099.robot2025.util.CustomLogger
+import edu.wpi.first.math.filter.Debouncer
+import org.team4099.lib.units.base.inSeconds
+import org.team4099.lib.units.derived.ElectricalPotential
+import org.team4099.lib.units.derived.inVolts
+import org.team4099.lib.units.derived.volts
+
+class Rollers(val io: RollersIO) {
+  val inputs = RollersIO.RollersIOInputs()
+
+  var currentState = RollersState.UNINITIALIZED
+  var currentRequest: Request.RollersRequest = Request.RollersRequest.OpenLoop(0.0.volts)
+    set(value) {
+      when (value) {
+        is Request.RollersRequest.OpenLoop -> {
+          rollersTargetVoltage = value.voltage
+        }
+        else -> {}
+      }
+      field = value
+    }
+
+  var lastRollerVoltageTarget = 0.0.volts
+  var lastRollerRunTime = Clock.fpgaTime
+
+  var debounceFilter = Debouncer(RollersConstants.BEAM_BREAK_FILTER_TIME.inSeconds)
+
+  val hasCoralVertical: Boolean
+    get() {
+      return inputs.rollerStatorCurrent > RollersConstants.CORAL_CURRENT_THRESHOLD &&
+        !inputs.rollerAppliedVoltage.epsilonEquals(
+          RollersConstants
+            .SCORE_CORAL_VOLTAGE
+        ) && // make sure score & intake are not the same voltage
+        (Clock.fpgaTime - lastRollerRunTime) >= RollersConstants.CORAL_DETECTION_TIME_THRESHOLD
+    }
+
+  val hasCoralHorizontal: Boolean
+    get() {
+      return inputs.rollerStatorCurrent > RollersConstants.CORAL_HORIZONTAL_CURRENT_THRESHOLD &&
+        inputs.rollerAppliedVoltage.sign < 0 &&
+        (Clock.fpgaTime - lastRollerRunTime) >=
+        RollersConstants.CORAL_HORIZONTAL_DETECTION_TIME_THRESHOLD
+    }
+
+  val hasAlgae: Boolean
+    get() {
+      return inputs.rollerStatorCurrent > RollersConstants.ALGAE_CURRENT_THRESHOLD &&
+        inputs.rollerAppliedVoltage.sign < 0 &&
+        (Clock.fpgaTime - lastRollerRunTime) >= RollersConstants.ALGAE_DETECTION_TIME_THRESHOLD
+    }
+
+  var rollersTargetVoltage: ElectricalPotential = 0.0.volts
+
+  fun periodic() {
+    io.updateInputs(inputs)
+    CustomLogger.processInputs("Rollers", inputs)
+    CustomLogger.recordOutput("Rollers/currentState", currentState.toString())
+
+    //    hasCoralVertical = debounceFilter.calculate(inputs.beamBroken)
+
+    var nextState = currentState
+    CustomLogger.recordOutput("Rollers/nextState", nextState.toString())
+    CustomLogger.recordOutput("Rollers/hasCoralVertical", hasCoralVertical)
+    CustomLogger.recordOutput("Rollers/targetVoltage", rollersTargetVoltage.inVolts)
+
+    when (currentState) {
+      RollersState.UNINITIALIZED -> {
+        nextState = fromRequestToState(currentRequest)
+      }
+      RollersState.OPEN_LOOP -> {
+        io.setVoltage(rollersTargetVoltage)
+        nextState = fromRequestToState(currentRequest)
+
+        if (lastRollerVoltageTarget != rollersTargetVoltage) {
+          lastRollerVoltageTarget = rollersTargetVoltage
+          lastRollerRunTime = Clock.fpgaTime
+        }
+
+        CustomLogger.recordOutput("Rollers/lastRollerRunTime", lastRollerRunTime.inSeconds)
+        CustomLogger.recordOutput("Rollers/delta", (Clock.fpgaTime - lastRollerRunTime).inSeconds)
+        CustomLogger.recordOutput("Rollers/surpassThreshold", (Clock.fpgaTime - lastRollerRunTime) >= RollersConstants.CORAL_DETECTION_TIME_THRESHOLD)
+      }
+    }
+
+    currentState = nextState
+  }
+
+  companion object {
+    enum class RollersState {
+      UNINITIALIZED,
+      OPEN_LOOP,
+    }
+
+    fun fromRequestToState(request: Request.RollersRequest): RollersState {
+      return RollersState.OPEN_LOOP
+    }
+  }
+}
