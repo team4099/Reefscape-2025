b4da82aaf6eb3730ede998cea8c653bd566cb306
yamamara <yamamrtyu@gmail.com>
2025-04-02 09:58:15 -0400

    Update dokka config to suppress inherited members

--- src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
+++ src/main/kotlin/com/team4099/robot2025/BuildConstants.kt
@@ -6,10 +6,10 @@ package com.team4099.robot2025
 const val MAVEN_GROUP = ""
 const val MAVEN_NAME = "Reefscape-2025"
 const val VERSION = "unspecified"
-const val GIT_REVISION = 92
-const val GIT_SHA = "35532d0ed06f8b87d4291342375cbb05c236cc85"
-const val GIT_DATE = "2025-03-16T12:27:47Z"
+const val GIT_REVISION = 93
+const val GIT_SHA = "267e1537e4a273d7847f87416b38bb42468b3ca2"
+const val GIT_DATE = "2025-03-28T19:38:26Z"
 const val GIT_BRANCH = "javadoc"
-const val BUILD_DATE = "2025-03-28T18:47:33Z"
-const val BUILD_UNIX_TIME = 1743205653839L
+const val BUILD_DATE = "2025-04-02T08:55:28Z"
+const val BUILD_UNIX_TIME = 1743602128678L
 const val DIRTY = 1
