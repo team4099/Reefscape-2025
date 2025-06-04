713a1c62fb2909ce4cc6765d3dc1efcf6f303c29
yamamara <yamamrtyu@gmail.com>
2025-06-04 09:22:14 -0400

    Update TrackTagCommand with *reef* aligning logic

--- src/main/kotlin/com/team4099/robot2025/commands/TrackTagCommand.kt
+++ src/main/kotlin/com/team4099/robot2025/commands/TrackTagCommand.kt
@@ -2,7 +2,6 @@ package com.team4099.robot2025.commands
 
 import com.team4099.lib.logging.LoggedTunableValue
 import com.team4099.robot2025.config.constants.DrivetrainConstants
-import com.team4099.robot2025.config.constants.VisionConstants
 import com.team4099.robot2025.subsystems.drivetrain.drive.Drivetrain
 import com.team4099.robot2025.subsystems.rollers.Ramp
 import com.team4099.robot2025.subsystems.superstructure.Request
@@ -12,13 +11,23 @@ import com.team4099.robot2025.util.driver.DriverProfile
 import edu.wpi.first.wpilibj.RobotBase
 import edu.wpi.first.wpilibj2.command.Command
 import org.team4099.lib.controller.PIDController
+import org.team4099.lib.geometry.Transform2d
+import org.team4099.lib.geometry.Translation2d
 import org.team4099.lib.units.Velocity
-import org.team4099.lib.units.derived.*
-import org.team4099.lib.units.inDegreesPerSecond
+import org.team4099.lib.units.base.meters
+import org.team4099.lib.units.derived.Radian
+import org.team4099.lib.units.derived.degrees
+import org.team4099.lib.units.derived.inDegrees
+import org.team4099.lib.units.derived.perDegree
+import org.team4099.lib.units.derived.inDegreesPerSecondPerDegree
+import org.team4099.lib.units.derived.inDegreesPerSecondPerDegreeSeconds
+import org.team4099.lib.units.derived.perDegreeSeconds
+import org.team4099.lib.units.derived.perDegreePerSecond
+import org.team4099.lib.units.derived.inDegreesPerSecondPerDegreePerSecond
+import org.team4099.lib.units.derived.radians
+import org.team4099.lib.units.derived.volts
 import org.team4099.lib.units.perSecond
 import kotlin.math.PI
-import kotlin.math.abs
-import kotlin.math.sign
 
 class TrackTagCommand(
   val driver: DriverProfile,
@@ -94,85 +103,113 @@ class TrackTagCommand(
   override fun initialize() {
     addRequirements(vision)
     thetaPID.reset()
+    vision.isAligned = false
   }
 
   override fun execute() {
-    val cams = vision.inputs
-    var aimedTowardsTag = false
+    val lastUpdate = vision.lastTrigVisionUpdate
+    val angleOffFromTag = (180.degrees - lastUpdate.robotTReefTag.rotation.absoluteValue) * lastUpdate.robotTReefTag.rotation.sign
+    CustomLogger.recordOutput("Vision/robotTTReefRotation", lastUpdate.robotTReefTag.rotation.inDegrees)
+    CustomLogger.recordOutput("Vision/angleOffFromTag", angleOffFromTag.inDegrees)
+    CustomLogger.recordOutput("Vision/isAligned", vision.isAligned)
+    val thetaFeedback = thetaPID.calculate(angleOffFromTag, 0.0.radians)
+    CustomLogger.recordOutput("Vision/thetaFeedback", thetaPID.error.inDegrees)
 
-    if (cams.size >= 2 && cams[0].cameraTargets.size > 0 && cams[1].cameraTargets.size > 0) {
-      val raven1 = cams[0].cameraTargets[0]
-      val raven2 = cams[1].cameraTargets[0]
-      // TODO: Make constant threshold
-      aimedTowardsTag = (raven2.bestCameraToTarget.rotation.z.radians.absoluteValue - raven1.bestCameraToTarget.rotation.z.radians.absoluteValue).inDegrees < 3 && raven1.fiducialId == raven2.fiducialId
-    }
-
-    CustomLogger.recordOutput("Vision/aimedTowardsTag", aimedTowardsTag)
-
-    if (aimedTowardsTag) {
-      Request.DrivetrainRequest.OpenLoop(
-        0.radians.perSecond,
-        driver.driveSpeedClampedSupplier(driveX, driveY, slowMode),
-        fieldOriented = true
-      )
+    if (!vision.isAligned && thetaPID.error.absoluteValue > 5.degrees && lastUpdate.targetTagID != -1) {
+      drivetrain.currentRequest =
+        Request.DrivetrainRequest.OpenLoop(
+          thetaFeedback,
+          driver.driveSpeedClampedSupplier(driveX, driveY, slowMode),
+          fieldOriented = true
+        )
     } else {
-      CustomLogger.recordDebugOutput("Vision/cameras", cams.size)
+      vision.isAligned = true
 
-      if (cams.size >= 2) {
-        val raven1HasTargets = cams[0].cameraTargets.size > 0
-        val raven2HasTargets = cams[1].cameraTargets.size > 0
-
-        if (raven1HasTargets || raven2HasTargets) {
-          val robotAngleFromTag =
-            if (raven1HasTargets && raven2HasTargets) {
-              val raven1Angle = cams[0].cameraTargets[0].bestCameraToTarget.rotation.z.radians
-              val raven2Angle = cams[1].cameraTargets[0].bestCameraToTarget.rotation.z.radians
-
-              raven2Angle.absoluteValue - raven1Angle.absoluteValue
-            }
-            else if (raven1HasTargets) {
-              val raven1Angle = cams[0].cameraTargets[0].bestCameraToTarget.rotation.z.radians
-              - (raven1Angle.absoluteValue - 50.degrees)
-            } else if (raven2HasTargets) {
-              val raven2Angle = cams[1].cameraTargets[0].bestCameraToTarget.rotation.z.radians
-              raven2Angle.absoluteValue - 50.degrees
-            }
-            else {
-              0.0.degrees
-            }
-
-          val thetaFeedback =
-            thetaPID.calculate(-robotAngleFromTag, 0.0.degrees)
-          isTargeting = true
-
-          CustomLogger.recordDebugOutput("Testing/error", thetaPID.error.inDegrees)
-          CustomLogger.recordDebugOutput("Testing/thetaFeedback", thetaFeedback.inDegreesPerSecond)
-          drivetrain.currentRequest =
-            Request.DrivetrainRequest.OpenLoop(
-              thetaFeedback,
-              driver.driveSpeedClampedSupplier(driveX, driveY, slowMode),
-              fieldOriented = true
-            )
-
-        } else {
-          drivetrain.currentRequest =
-            Request.DrivetrainRequest.OpenLoop(
-              0.0.radians.perSecond,
-              driver.driveSpeedClampedSupplier(driveX, driveY, slowMode),
-              fieldOriented = true
-            )
-        }
-      }
+      drivetrain.currentRequest =
+        Request.DrivetrainRequest.OpenLoop(
+          0.0.radians.perSecond,
+          driver.driveSpeedClampedSupplier(driveX, driveY, slowMode),
+          fieldOriented = true
+        )
     }
+//    if (cams.size >= 2 && cams[0].cameraTargets.size > 0 && cams[1].cameraTargets.size > 0) {
+//      val raven1 = cams[0].cameraTargets[0]
+//      val raven2 = cams[1].cameraTargets[0]
+//      // TODO: Make constant threshold
+//      aimedTowardsTag = (raven2.bestCameraToTarget.rotation.z.radians.absoluteValue - raven1.bestCameraToTarget.rotation.z.radians.absoluteValue).inDegrees < 3 && raven1.fiducialId == raven2.fiducialId
+//    }
+//
+//    CustomLogger.recordOutput("Vision/aimedTowardsTag", aimedTowardsTag)
+//
+//    if (aimedTowardsTag) {
+//      Request.DrivetrainRequest.OpenLoop(
+//        0.radians.perSecond,
+//        driver.driveSpeedClampedSupplier(driveX, driveY, slowMode),
+//        fieldOriented = true
+//      )
+//    } else {
+//      CustomLogger.recordDebugOutput("Vision/cameras", cams.size)
+//
+//      if (cams.size >= 2) {
+//        val raven1HasTargets = cams[0].cameraTargets.size > 0
+//        val raven2HasTargets = cams[1].cameraTargets.size > 0
+//
+//        if (raven1HasTargets || raven2HasTargets) {
+//          val robotAngleFromTag =
+//            if (raven1HasTargets && raven2HasTargets) {
+//              val raven1Angle = cams[0].cameraTargets[0].bestCameraToTarget.rotation.z.radians
+//              val raven2Angle = cams[1].cameraTargets[0].bestCameraToTarget.rotation.z.radians
+//
+//              raven2Angle.absoluteValue - raven1Angle.absoluteValue
+//            }
+//            else if (raven1HasTargets) {
+//              val raven1Angle = cams[0].cameraTargets[0].bestCameraToTarget.rotation.z.radians
+//              - (raven1Angle.absoluteValue - 50.degrees)
+//            } else if (raven2HasTargets) {
+//              val raven2Angle = cams[1].cameraTargets[0].bestCameraToTarget.rotation.z.radians
+//              raven2Angle.absoluteValue - 50.degrees
+//            }
+//            else {
+//              0.0.degrees
+//            }
+//
+//          val thetaFeedback =
+//            thetaPID.calculate(-robotAngleFromTag, 0.0.degrees)
+//          isTargeting = true
+//
+//          CustomLogger.recordDebugOutput("Testing/error", thetaPID.error.inDegrees)
+//          CustomLogger.recordDebugOutput("Testing/thetaFeedback", thetaFeedback.inDegreesPerSecond)
+//          drivetrain.currentRequest =
+//            Request.DrivetrainRequest.OpenLoop(
+//              thetaFeedback,
+//              driver.driveSpeedClampedSupplier(driveX, driveY, slowMode),
+//              fieldOriented = true
+//            )
+//
+//        } else {
+//          drivetrain.currentRequest =
+//            Request.DrivetrainRequest.OpenLoop(
+//              0.0.radians.perSecond,
+//              driver.driveSpeedClampedSupplier(driveX, driveY, slowMode),
+//              fieldOriented = true
+//            )
+//        }
+//      }
+//    }
 
     // CustomLogger.recordOutput("Vision/loggedRobotAngleFromTag", robotAngleFromTag)
   }
 
   override fun isFinished(): Boolean {
-    return thetaPID.error.absoluteValue < 2.degrees && isTargeting
+    return vision.isAligned
   }
 
   override fun end(interrupted: Boolean) {
-    ramp.currentRequest = Request.RampRequest.OpenLoop(0.0.volts)
+    drivetrain.currentRequest =
+      Request.DrivetrainRequest.OpenLoop(
+        driver.rotationSpeedClampedSupplier(turn, slowMode),
+        driver.driveSpeedClampedSupplier(driveX, driveY, slowMode),
+        fieldOriented = true
+      )
   }
 }
